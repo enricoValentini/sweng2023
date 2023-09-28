09dea0d76f0166f8ae2944709ea82bbf5e622443
DawidPiesla <dawid5052000@gmail.com>
2023-09-28 17:43:43 +0200

    Prova commit

--- sweng-client/src/main/java/sweng2023/sweng/App.java
+++ sweng-client/src/main/java/sweng2023/sweng/App.java
@@ -1,5 +1,7 @@
 package sweng2023.sweng;
 
+//enri  e carino
+
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
